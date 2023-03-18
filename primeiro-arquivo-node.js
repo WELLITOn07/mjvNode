4462f4881276929e97abd4e3d341de88e2f78583
WELLITOn07 <97143231+WELLITOn07@users.noreply.github.com>
2023-03-18 15:05:00 -0300

    feat(primeiro-arquivo-node): adding the phrase `Hello World` and a function to add two variables of type number

---
+++ primeiro-arquivo-node.js
@@ -0,0 +1,15 @@
+console.log('Hello World!');
+
+
+/**@type {number} */
+const numberOne = 2;
+/**@type {number} */
+const numberTwo = 2;
+
+
+/**@returns {number} */
+const some = (n1, n2) => {
+    return n1 + n2
+};
+
+console.log(`A soma de numberOne e numberTwo é: ${some(numberOne, numberTwo)}`);
